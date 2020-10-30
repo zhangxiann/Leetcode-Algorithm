98f5eb2c5cdc6f28d8b0d9fc958fa158a4bf209f
张贤 <1756569043@qq.com>
2020-10-30 22:43:13 +0800

    solve 345

--- src/Solution345.java
+++ src/Solution345.java
@@ -2,8 +2,31 @@
 // https://leetcode.com/problems/reverse-vowels-of-a-string/description/
 
 
-public class Solution345 {
-    public String reverseVowels(String s) {
+import java.util.Arrays;
+import java.util.HashSet;
 
+public class Solution345 {
+    private final static HashSet<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+
+    public String reverseVowels(String s) {
+        if (s == null || s.length() <= 1) return s;
+        char[] chars = s.toCharArray();
+        int left = 0, right = chars.length - 1;
+        while (left < right) {
+            while (left < right && !vowels.contains(chars[left])) {
+                left++;
+            }
+            while (left < right && !vowels.contains(chars[right])) {
+                right--;
+            }
+            if (left < right) {
+                char tmp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = tmp;
+            }
+            left++;
+            right--;
+        }
+        return new String(chars);
     }
 }
