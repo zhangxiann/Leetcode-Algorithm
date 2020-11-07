0ec51f0b40309a6c0340dfc547e41817d4f52e8e
张贤 <1756569043@qq.com>
2020-11-07 20:53:52 +0800

    add 122

---
+++ src/Solution122.java
@@ -0,0 +1,12 @@
+public class Solution122 {
+    public int maxProfit(int[] prices) {
+        if (prices==null || prices.length<2)
+            return 0;
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i]>prices[i-1])
+                profit += (prices[i]-prices[i-1]);
+        }
+        return profit;
+    }
+}
