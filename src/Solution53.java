0adfec16320a8a6f512d3f7c94f86f5035582981
张贤 <1756569043@qq.com>
2020-11-07 21:04:57 +0800

    add 53

---
+++ src/Solution53.java
@@ -0,0 +1,13 @@
+public class Solution53 {
+    public int maxSubArray(int[] nums) {
+        if (nums==null || nums.length==0)
+            return 0;
+        int preSum=nums[0];
+        int maxSum=preSum;
+        for (int i = 1; i < nums.length; i++) {
+            preSum = preSum>0 ? preSum+nums[i] : nums[i];
+            maxSum = Math.max(maxSum, preSum);
+        }
+        return maxSum;
+    }
+}
