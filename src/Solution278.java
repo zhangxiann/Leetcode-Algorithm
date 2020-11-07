4b0036ba76cd7a89c441b8a507b0829f887914b8
张贤 <1756569043@qq.com>
2020-11-07 22:27:11 +0800

    add 278

---
+++ src/Solution278.java
@@ -0,0 +1,13 @@
+public class Solution278 {
+    public int firstBadVersion(int n) {
+        int l=1, h=n;
+        while (l<h){
+            int mid = l+(h-l)/2;
+            if (isBadVersion(mid))
+                h=mid;
+            else
+                l=mid+1;
+        }
+        return h;
+    }
+}
