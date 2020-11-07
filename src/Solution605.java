cb721bb92ea0c302afc13ecb2e654741e18bc6f1
张贤 <1756569043@qq.com>
2020-11-07 15:05:21 +0800

    add 605

---
+++ src/Solution605.java
@@ -0,0 +1,12 @@
+public class Solution605 {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count=0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i]==0 && (i==0 || flowerbed[i-1]==0) && (i==flowerbed.length-1 || flowerbed[i+1]==0)){
+                count++;
+                flowerbed[i]=1;
+            }
+        }
+        return count>=n;
+    }
+}
